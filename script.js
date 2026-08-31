28068dcf58d48172ae23c894cddc9fefb3977f83
priyanshu ahir <priyanshuahir777@gmail.com>
2026-08-31 13:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+const video = document.querySelector(".flex");
+const toggle = document.querySelector(".toggle");
+const volume = document.querySelector(".volume");
+const playSpeed = document.querySelector(".playbackSpeed");
+const skipButtons = document.querySelector("[data-skip]");
+
+function togglePlay(){
+	if(video.paused){
+		video.play();
+	}else{
+		video.pause();
+	}
+}
+
+function updateButton() {
+	toggle.textContent = video.pause ? '►' : '❚ ❚';
+}
+
+function handleVolume() {
+	video.volume = this.value;
+}
+
+function handleSpeed(){
+	video.playRate = this.value;
+}
+
+function skip(){
+	video.currentTime += Number(this.dataset.skip);
+}
+
+
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+volume.addEventListener('input', handleVolume);
+playbackSpeed.addEventListener('input', handleSpeed);
+skipButtons.forEach(button =>{
+	button.addEventListener('click', skip);
+	});
