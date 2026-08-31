3f688d08b6c7d2b713e0a20414d4521d2386a4ff
priyanshu ahir <priyanshuahir777@gmail.com>
2026-08-31 13:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,67 @@
 const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
-
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-
-const video = document.querySelector(".flex");
-const toggle = document.querySelector(".toggle");
-const volume = document.querySelector(".volume");
-const playSpeed = document.querySelector(".playbackSpeed");
-const skipButtons = document.querySelector("[data-skip]");
-
-function togglePlay(){
-	if(video.paused){
-		video.play();
-	}else{
-		video.pause();
-	}
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+// NEW CODE
+
+const video = document.querySelector('.flex');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('.volume');
+const playbackSpeed = document.querySelector('.playbackSpeed');
+const skipButtons = document.querySelectorAll('[data-skip]');
+
+
+// Play / Pause
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+
+// Change button
 function updateButton() {
-	toggle.textContent = video.pause ? '►' : '❚ ❚';
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
 }
 
+
+// Volume
 function handleVolume() {
-	video.volume = this.value;
-}
-
-function handleSpeed(){
-	video.playRate = this.value;
-}
-
-function skip(){
-	video.currentTime += Number(this.dataset.skip);
+  video.volume = this.value;
 }
 
 
+// Playback speed
+function handleSpeed() {
+  video.playbackRate = this.value;
+}
+
+
+// Skip
+function skip() {
+  video.currentTime += Number(this.dataset.skip);
+}
+
+
+// Event listeners
 toggle.addEventListener('click', togglePlay);
+
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 volume.addEventListener('input', handleVolume);
+
 playbackSpeed.addEventListener('input', handleSpeed);
-skipButtons.forEach(button =>{
-	button.addEventListener('click', skip);
-	});
+
+skipButtons.forEach(button => {
+  button.addEventListener('click', skip);
+});
